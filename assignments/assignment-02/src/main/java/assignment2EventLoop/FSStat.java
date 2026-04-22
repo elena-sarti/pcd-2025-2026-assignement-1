51bf44e65c600c8e9d7ff70c36e9ff36b120e911
Elena Sarti <elena.sarti8@studio.unibo.it>
2026-04-22 16:25:58 +0200

    refactoring

--- assignments/assignment-02/src/main/java/assignment2EventLoop/FSStat.java
+++ assignments/assignment-02/src/main/java/assignment2EventLoop/FSStat.java
@@ -1,4 +1,4 @@
-package assignment2;
+package assignment2EventLoop;
 
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
--- assignments/assignment-02/src/main/java/assignment2EventLoop/Test_FSStat.java
+++ assignments/assignment-02/src/main/java/assignment2EventLoop/Test_FSStat.java
@@ -1,4 +1,4 @@
-package assignment2;
+package assignment2EventLoop;
 
 import io.vertx.core.Vertx;
 
@@ -6,7 +6,7 @@ public class Test_FSStat {
     public static void main(String[] args){
         Vertx vertx = Vertx.vertx();
         FSStat library = new FSStat(vertx);
-        String d = "C:\\Users\\Elena\\Desktop\\PCD\\pcd-2025-2026-assignement-1\\assignments\\assignment-02\\target\\classes\\assignment2";
+        String d = "C:\\Users\\Elena\\Desktop\\PCD\\pcd-2025-2026-assignement-1\\assignments\\assignment-02\\target\\classes\\assignment2EventLoop";
         library
             .getFSReport(d, 2, 4)
             .onSuccess(report -> System.out.println(report.toString()));
